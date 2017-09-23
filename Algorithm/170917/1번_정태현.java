f86482d015ffc936511135b7f949677402b2d31f
WilsonParkers <xogus0790@naver.com>
2017-09-23 12:19:10 +0900

    Update 1번_정태현.java

--- Algorithm/170917/1번_정태현.java
+++ Algorithm/170917/1번_정태현.java
@@ -1,1 +1,34 @@
+package kakao_test;
 
+public class Test_1 {
+
+    // Success
+    public static int solution(int n) {
+        int answer = 0;
+        String s = n + "";
+        for (char c : s.toCharArray()) {
+            answer += Integer.parseInt(c + "");
+        }
+        return answer;
+    }
+
+    public static void main(String[] args) {
+
+        System.out.println(solution(15));
+    }
+
+    // Answer
+    public int answer(int n) {
+
+        int answer = 0;
+
+        while (n > 0) {
+
+            answer += (n % 10);
+            n /= 10;
+        }
+
+        return answer;
+    }
+
+}
